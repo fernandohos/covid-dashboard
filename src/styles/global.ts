8370fd21cb6d842914e80cb658117ea812b4279c
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2021-10-22 14:23:37 -0300

    colors variables

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,13 +4,33 @@ export const GlobalStyle = createGlobalStyle`
     * {
         padding: 0;
         margin: 0;
+        font-family: 'Poppins', sans-serif;
     }
     html {
         --background: #1B283A;
         --card-background: #243143;
+        --chart-card-background: #2D3A4C;
         --gradient: linear-gradient(135deg, #ff6701, #ff0973);
+        --text-color: #fff;
+        --confirmed-font-color: #44ff44;
+        --deaths-font-color: #ff4444;
+        font-size: 62.5%;
     }
     body {
-        background: var(--gradient);
+        font-size: 2.0rem;
+        background: var(--background);
+        color: var(--text-color)
+    }
+
+    @media (prefers-color-scheme: light) {
+        html {
+            --background: #F3F6FF;
+            --card-background: #fff;
+            --chart-card-background: #FCFCFC;
+            --gradient: linear-gradient(135deg, #ff6701, #ff0973);
+            --text-color: #2b2b2b;
+            --confirmed-font-color: #44ff44;
+            --deaths-font-color: #ff4444;
+        }
     }
 `;
