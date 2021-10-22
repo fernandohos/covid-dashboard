78893876925accc3d12b0deebaef301935cda22f
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2021-10-22 19:30:54 -0300

    world stats

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,20 @@ import * as C from './styles/App';
 function App() {
   return (
     <C.Container>
-      Hello World!
+      <C.Title>Covid-19 Dashboard</C.Title>
+      <C.WorldStats>
+        <h2>World</h2>
+        <C.CasesWrapper>
+          <div>
+            <h3>confirmed</h3>
+            <p>240.630.839</p>
+          </div>
+          <div>
+            <h3>deahts</h3>
+            <p>4.890.776</p>
+          </div>
+        </C.CasesWrapper>
+      </C.WorldStats>
     </C.Container>
   );
 }
--- src/styles/App.ts
+++ src/styles/App.ts
@@ -1,5 +1,56 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-    
+    max-width: 120rem;
+    margin: auto;
+`;
+
+export const Title = styled.h1`
+    color: var(--text-color);
+    font-size: 2.8rem;
+    text-align: center;
+    font-weight: 500;
+    margin: 2.5rem auto;
+`;
+
+export const WorldStats = styled.div`
+    max-width: 600px;
+    width: 100%;
+    text-align: center;
+    margin: auto;
+
+    h2 {
+        font-weight: 400;
+        font-size: 2.4rem;
+        margin-top: 1rem;
+    }
+`;
+
+export const CasesWrapper = styled.div`
+    display: flex;
+
+    div {
+        flex: 1;
+
+        h3 {
+            font-size: 1.5rem;
+            line-height: 1.5rem;
+            font-weight: 300;
+            margin: 0;
+            color: var(--text-color);
+        }
+        p {
+            font-size: 2.5rem;
+        }
+        
+        &:nth-child(1) {
+            color: var(--confirmed-font-color);
+            text-align: left;
+        }
+        &:nth-child(2) {
+            color: var(--deaths-font-color);
+            text-align: right;
+        }
+    }
+
 `;
