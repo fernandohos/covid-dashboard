d6302971a2c638121bd931fbadf02ad670a90d27
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2021-10-25 21:38:08 -0300

    country type

---
+++ src/types/CountryType.ts
@@ -0,0 +1,9 @@
+export type CountryType = {
+    country: string;
+    abbreviation: string;
+    confirmed: number;
+    deaths: number;
+    continent: string;
+    updated: string;
+    population: number;
+}
