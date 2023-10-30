079869923f14e6de4da19418a1efd261940f996f
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2023-10-30 20:22:16 -0300

    add country provider

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import App from "./App";
 import { GlobalStyle } from "./styles/global";
 import CountriesProvider from "./context/CountriesContext";
 import { createRoot } from "react-dom/client";
+import { CountryProvider } from "./context/CountryContext";
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
@@ -10,8 +11,10 @@ const root = createRoot(container!);
 root.render(
   <React.StrictMode>
     <CountriesProvider>
-      <GlobalStyle />
-      <App />
+      <CountryProvider>
+        <GlobalStyle />
+        <App />
+      </CountryProvider>
     </CountriesProvider>
   </React.StrictMode>
 );
